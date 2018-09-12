4df4048bd64c054b5d53f532d54d5ebdf87c7f06
Guilherme Almino Carvalho <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2018-09-12 11:33:37 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 int main(void)
 {
 	int i;
-
+	
+	printf("|Inteiro|Caractere|Hexadecimal|Octadecimal\n");
 	for (i = 0; i <= 255; i++)
 	{
-		printf("%d - %c\n", i, i);
+		printf("|%d|%c|%x|%o|\n", i, i, i, i);
 	}
 
 	return 0;
